009d6043a88bf726d520bb785b302908119b88af
thumbe12856 <thumbd12856@gmail.com>
2024-02-03 20:53:44 +0800

    Add dropbear package and set root password.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 'ad5a8cd8700607fa296737e885ca693d7682776c'
+AESD_ASSIGNMENTS_VERSION = '06f1f0072ab4053e967d0652cf1a3eac8a5163e0'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -19,12 +19,12 @@ endef
 
 # Add writer, finder and finder-test utilities/scripts to the installation steps.
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder* $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/root/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/root/conf/
 	mkdir -p $(TARGET_DIR)/root/.ssh/
 	$(INSTALL) -m 0755 $(TARGET_DIR)/../../../conf/authorized_keys $(TARGET_DIR)/root/.ssh/
 endef
