b421909880372ca7fdd76138c928c77736ebc941
thumbe12856 <thumbd12856@gmail.com>
2024-02-09 22:55:52 +0800

    Execute aesdsocket-start-stop as daemon mode in buildroot.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '06f1f0072ab4053e967d0652cf1a3eac8a5163e0'
+AESD_ASSIGNMENTS_VERSION = 'ab8a418f2f8e50e4d0ec8de959c28fbd10876de2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,17 +14,14 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # Add writer, finder and finder-test utilities/scripts to the installation steps.
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder* $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/root/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/root/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment5/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0777 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0777 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	mkdir -p $(TARGET_DIR)/root/.ssh/
 	$(INSTALL) -m 0755 $(TARGET_DIR)/../../../conf/authorized_keys $(TARGET_DIR)/root/.ssh/
 endef
